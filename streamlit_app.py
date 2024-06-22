e62a039f6d8b93eaf93b87afc85ff78c1647e33d
Udaykiran2022 <99736207+Udaykiran2022@users.noreply.github.com>
2024-06-22 17:33:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import necessary libraries
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import snowflake.snowpark
 
